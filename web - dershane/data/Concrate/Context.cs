069942c43b24de11fab3c54613f58406bef84f34
ömer hasan <omerhasangulsoy@hotmail.com>
2024-04-09 18:05:56 +0300

    Identity
    Identity kütüphanesinin eklenmesi ve tabloların özelleştirilmesi.

---
+++ web - dershane/data/Concrate/Context.cs
@@ -0,0 +1,19 @@
+﻿using entity.Concrate;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace data.Concrate
+{
+	public class Context : IdentityDbContext<AppUser, AppRole, int>
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlServer("server=ELITEBOOK\\MSSQLSERVER2012;database=DershaneDB;integrated security=true");
+		}
+	}
+}
