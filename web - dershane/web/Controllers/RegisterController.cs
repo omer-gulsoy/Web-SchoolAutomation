c9277d805f0588f202bb2a511f15b1bff840d67d
ömer hasan <omerhasangulsoy@hotmail.com>
2024-04-09 21:19:35 +0300

    Identity
    Identity sağlamak için Login ve Register Controllerlarının kurulumu yapıldı. Login ve Register Sayfalarına gerekli fonksiyonlar eklendi ve tasarımları tamamlandı.

---
+++ web - dershane/web/Controllers/RegisterController.cs
@@ -0,0 +1,48 @@
+﻿using dto.dtos.AppUserDtos;
+using entity.Concrate;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace web.Controllers
+{
+	public class RegisterController : Controller
+	{
+		private readonly UserManager<AppUser> _userManager;
+
+		public RegisterController(UserManager<AppUser> userManager)
+		{
+			_userManager = userManager;
+		}
+		[HttpGet]
+		public IActionResult Index()
+		{
+			return View();
+		}
+		[HttpPost]
+		public async Task<IActionResult> Index(AppUserRegisterDto appUserRegisterDto)
+		{
+			if (ModelState.IsValid)
+			{
+				AppUser appUser = new AppUser
+				{
+					UserName = appUserRegisterDto.UserName,
+					Name = appUserRegisterDto.Name,
+					Surname = appUserRegisterDto.Surname,
+				};
+				var result = await _userManager.CreateAsync(appUser, appUserRegisterDto.Password);
+				if (result.Succeeded)
+				{
+					return RedirectToAction("Index", "Login");
+				}
+				else
+				{
+					foreach (var item in result.Errors)
+					{
+						ModelState.AddModelError("", item.Description);
+					}
+				}
+			}
+			return View();
+		}
+	}
+}
---
+++ web - dershane/web/Controllers/TeachersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace web.Controllers
+{
+	public class TeachersController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
