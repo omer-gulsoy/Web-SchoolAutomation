1989f67bfb0652babccb579f18456e93baa155c1
ömer hasan <omerhasangulsoy@hotmail.com>
2024-04-19 10:59:13 +0300

    Sınıf İşlemleri
    Sınıf işlemlerinin fonksiyonları arayüz tasarımları tamamlandı. Testler yapıldı.

--- web - dershane/web/Areas/Admin/Controllers/ClassController.cs
+++ web - dershane/web/Areas/Admin/Controllers/ClassController.cs
@@ -1,4 +1,5 @@
 ﻿using data.Concrate;
+using entity.Concrate;
 using Microsoft.AspNetCore.Mvc;
 
 namespace web.Areas.Admin.Controllers
@@ -12,6 +13,40 @@ namespace web.Areas.Admin.Controllers
 		{
 			var degerler = Context.Classes.ToList();
 			return View(degerler);
+		}
+		[HttpGet]
+		public IActionResult ClassAdd()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult ClassAdd(Class c)
+		{
+			Context.Classes.Add(c);
+			Context.SaveChanges();
+			return RedirectToAction("Index", "Class");
+		}
+		public IActionResult ClassDelete(int id)
+		{
+			var silinecek = Context.Classes.Find(id);
+			Context.Classes.Remove(silinecek);
+			Context.SaveChanges();
+			return RedirectToAction("Index", "Class");
+		}
+		public IActionResult ClassGet(int id)
+		{
+			var getirilecek = Context.Classes.Find(id);
+			return View("ClassGet", getirilecek);
+		}
+		public IActionResult ClassUpdate(Class c)
+		{
+			var guncellenecek = Context.Classes.Find(c.Class_Id);
+			guncellenecek.Class_Id = c.Class_Id;
+			guncellenecek.Number = c.Number;
+			guncellenecek.Branch = c.Branch;
+			guncellenecek.Status = c.Status;
+			Context.SaveChanges();
+			return RedirectToAction("Index", "Class");
 		}
 	}
 }
